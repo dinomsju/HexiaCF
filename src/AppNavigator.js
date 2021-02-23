0a07b443bb5b7acaefc7ba8a20ca949beb2ab856
dinomsju <dinomsju@gmail.com>
2021-02-23 09:38:43 +0700

    update

--- src/AppNavigator.js
+++ src/AppNavigator.js
@@ -1,19 +1,16 @@
 import * as React from 'react';
 import 'react-native-gesture-handler';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
 import BottomNavigation from './BottomNavigation';
-import Login from './components/screens/Login'
-import Signup from './components/screens/Signup'
+import Login from './components/screens/Login';
+import Signup from './components/screens/Signup';
 const Stack = createStackNavigator();
 
 function StackNavigator() {
   return (
     <Stack.Navigator headerMode="none" initialRouteName="Login">
-      <Stack.Screen
-        name="Home"
-        component={BottomNavigation}
-      />
+      <Stack.Screen name="Home" component={BottomNavigation} />
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="Signup" component={Signup} />
     </Stack.Navigator>
--- src/BottomNavigation.js
+++ src/BottomNavigation.js
@@ -1,78 +1,127 @@
 import * as React from 'react';
 
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Icon from 'react-native-vector-icons/Feather';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-import Home from './components/tabscreens/Home'
-import Profile from './components/tabscreens/Profile'
-import Setting from './components/tabscreens/Setting'
-import Notification from './components/tabscreens/Notification'
+//Vector Icons
+import Foundation from 'react-native-vector-icons/Foundation';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+import Home from './components/tabscreens/Home';
+import Profile from './components/tabscreens/Profile';
+import Setting from './components/tabscreens/Setting';
+import Notification from './components/tabscreens/Notification';
+
+//Constants
+import {COLORS, icons} from './constants';
+import Svg, {Path} from 'react-native-svg';
+import {TouchableOpacity, View} from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
-const tabBarOptions = {
-  activeTintColor: '#000',
-  showLabel: false,
-  // keyboardHidesTabBar: true,
-  // style: {
-  //   position: 'absolute',
-  // },
+const TabBarCustomButton = ({accessibilityState, children, onPress}) => {
+  var isSelected = accessibilityState.selected;
+
+  if (isSelected) {
+    return (
+      <View style={{flex: 1, alignItems: 'center'}}>
+        <View style={{flexDirection: 'row', position: 'absolute', top: 0}}>
+          <View style={{flex: 1, backgroundColor: COLORS.white}}></View>
+          <Svg width={75} height={61} viewBox="0 0 75 61">
+            <Path
+              d="M75.2 0v61H0V0c4.1 0 7.4 3.1 7.9 7.1C10 21.7 22.5 33 37.7 33c15.2 0 27.7-11.3 29.7-25.9.5-4 3.9-7.1 7.9-7.1h-.1z"
+              fill={COLORS.white}
+            />
+          </Svg>
+          <View style={{flex: 1, backgroundColor: COLORS.white}}></View>
+        </View>
+
+        <TouchableOpacity
+          style={{
+            top: -22.5,
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: 50,
+            height: 50,
+            borderRadius: 25,
+            backgroundColor: COLORS.white,
+          }}
+          onPress={onPress}>
+          {children}
+        </TouchableOpacity>
+      </View>
+    );
+  } else {
+    return (
+      <TouchableOpacity
+        style={{
+          flex: 1,
+          height: 50,
+          backgroundColor: COLORS.white,
+        }}
+        activeOpacity={1}
+        onPress={onPress}>
+        {children}
+      </TouchableOpacity>
+    );
+  }
 };
-function MyBottomTab() {
+
+const Tabs = () => {
   return (
-    <Tab.Navigator tabBarOptions={tabBarOptions}>
+    <Tab.Navigator
+      tabBarOptions={{
+        showLabel: false,
+        style: {
+          borderTopWidth: 0,
+          backgroundColor: 'transparent',
+          elevation: 0,
+        },
+        activeTintColor: '#243D8C',
+      }}>
       <Tab.Screen
         name="Home"
         component={Home}
         options={{
-          tabBarIcon: ({ color, size }) => (
-            <Icon name="film" color={color} size={size} />
+          tabBarIcon: ({color, size}) => (
+            <Foundation name="home" color={color} size={size} />
           ),
+          tabBarButton: (props) => <TabBarCustomButton {...props} />,
         }}
       />
       <Tab.Screen
-        name="Setting"
-        component={Setting}
-        options={{
-          tabBarIcon: ({ color, size }) => (
-            <Icon name="music" color={color} size={size} />
-          ),
-        }}
-      />
-      {/* <Tab.Screen
-        name={ROUTE_KEY.Category}
-        component={Category}
-        options={{
-          tabBarIcon: ({ color, size }) => (
-            <Icon name="grid" color={color} size={size} />
-          ),
-        }}
-      /> */}
-      <Tab.Screen
-        name="Notification"
-        component={Notification}
-        options={{
-          tabBarIcon: ({ color, size }) => (
-            <Icon name="heart" color={color} size={size} />
-          ),
-          tabBarBadge: 1,
-        }}
-      />
-
-      <Tab.Screen
-        name="Profile"
+        name="Search"
         component={Profile}
         options={{
-          tabBarIcon: ({ color, size }) => (
-            <Icon name="user" color={color} size={size} />
+          tabBarIcon: ({color, size}) => (
+            <FontAwesome name="search" color={color} size={size} />
           ),
+          tabBarButton: (props) => <TabBarCustomButton {...props} />,
+        }}
+      />
+      <Tab.Screen
+        name="Notifications"
+        component={Notification}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <Ionicons name="notifications" color={color} size={size} />
+          ),
+          tabBarButton: (props) => <TabBarCustomButton {...props} />,
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={Setting}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <Ionicons name="settings" color={color} size={size} />
+          ),
+          tabBarButton: (props) => <TabBarCustomButton {...props} />,
         }}
       />
     </Tab.Navigator>
   );
-}
+};
 
-export default function BottomNavigation() {
-  return <MyBottomTab />;
-}
+export default Tabs;
